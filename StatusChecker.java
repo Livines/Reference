a949c4e3fe3d893196ce40491cfd77a7b3d19b0e
Livin <livines068@gmail.com>
2017-11-23 12:48:01 +0530

    First commit

---
+++ StatusChecker.java
@@ -0,0 +1,63 @@
+package com.mausam.weather.india.util;
+
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Build;
+import android.provider.Settings;
+import android.text.TextUtils;
+
+public class StatusChecker {
+    private static StatusChecker instance;
+    private Context mContext;
+
+    private StatusChecker(Context mContext) {
+        this.mContext=mContext;
+    }
+
+    public static synchronized StatusChecker getInstance(Context mContext){
+        if(instance==null){
+            instance = new StatusChecker(mContext.getApplicationContext());
+        }
+        return instance;
+    }
+
+    public boolean isLocationEnabled() {
+        int locationMode = 0;
+        String locationProviders;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT){
+            try {
+                locationMode = Settings.Secure.getInt(mContext.getContentResolver(), Settings.Secure.LOCATION_MODE);
+            } catch (Settings.SettingNotFoundException e) {
+                e.printStackTrace();
+            }
+            return locationMode != Settings.Secure.LOCATION_MODE_OFF;
+        }else{
+            locationProviders = Settings.Secure.getString(mContext.getContentResolver(), Settings.Secure.LOCATION_PROVIDERS_ALLOWED);
+            return !TextUtils.isEmpty(locationProviders);
+        }
+    }
+
+    public boolean gpsStatus() {
+        final LocationManager manager = (LocationManager)mContext.getSystemService(Context.LOCATION_SERVICE );
+        if ( !manager.isProviderEnabled( LocationManager.GPS_PROVIDER ) ) return false;
+        else return true;
+    }
+
+    public boolean isNetworkAvailable() {
+        ConnectivityManager cm = (ConnectivityManager) mContext.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = cm.getActiveNetworkInfo();
+        // if no network is available networkInfo will be null
+        // otherwise check if we are connected
+        if (networkInfo != null && networkInfo.isConnected()) {
+            return true;
+        }
+        return false;
+    }
+
+
+}
